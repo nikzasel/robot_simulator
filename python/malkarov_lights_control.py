ec1d939729d6c033d726402ab7375b496a1cbf85
MrMalkarov <mr.clumber.park@gmail.com>
2019-12-23 20:19:48 +0300

    Дополнительные изменения

--- python/malkarov_lights_control.py
+++ python/malkarov_lights_control.py
@@ -12,7 +12,6 @@ print ('Program started')
 vrep.simxFinish(-1)
 clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
 
-
 if clientID!=-1:
     #Если подключение произошло успешно...
     print ('Connected to remote API server')
